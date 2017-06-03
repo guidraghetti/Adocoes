58c0bd47435b5c493b1953503f155ad6e94e6b1d
Bruno Reis <13202021@pucrs.br>
2017-06-03 13:05:38 -0300

    converting to IATE wip

--- api/src/Menor/Entity.js
+++ api/src/Menor/Entity.js
@@ -13,17 +13,115 @@ export default class Entity {
 		const adapter = new this.Adapter()
 		return adapter.fetchAll()
 	}
+	
+	find(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetch(body.id)
+	}
+	
+	remove(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.delete(body.id)
+	}
+	
+	update(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetchAndUpdate(body)
+	}
+	
+	getOrdination(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetchOrdination()
+	}
+	
+	addIntersting(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.addIntersting()		
+	}
+	
+	fetchAllIntersting(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetchAllInstersting()
+	}
+	
+	removeIntersting(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.removeIntersting();		
+	}
+	
+	createImage(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.createImage();
+	}
+	
+	fetchImages(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetchAllImage();
+	}
+	
+	fetchImage(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetchImage();
+	}
+	
+	removeImage(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.removeImage();
+	}
+		
+	createImage(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.createImage();
+	}
+	
+	createVideo(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.createVideo();
+	}
+	
+	fetchAllVideo(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetchAllVideo();
+	}
+	
+	fetchVideo(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.fetchVideo();
+	}
+	
+	removeVideo(body) {
+		const adapter = new this.Adapter()
+		
+		return adapter.removeVideo()
+		
+	}
+	
 
 	validateToken(body) {
 		return new Promise((resolve, reject) => {
 			resolve(body)
 		})
-	}
+	}	
 	
 	validate(body, schema) {
 
 
-			const schema = Joi.object(){
+		const schema = Joi.object(){
 		name = Joi.string().require()
 		gender = Joi.string().require().regex(/M|F/)
         birthDay = Joi.date().require().max('now').min('now') //somar data atual + 18 anos
