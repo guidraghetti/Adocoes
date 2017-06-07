9d4900a39aef63bd571c9f30e694154f10ebc6f9
Rafael Companhoni <companhoni@gmail.com>
2017-06-07 11:12:28 -0300

    Correção na senha do cliente inicial no script Mongo

--- model/inicializacaoMongo.js
+++ model/inicializacaoMongo.js
@@ -10,7 +10,7 @@ use Adocoes
 
 db.usuarios.save({
     email: "eduardo.arruda@doc.space",
-    senha: "$2a$05$M1COhDus0bY6rqkD2REl8.ZccrFgzFXH0a7sOEYU/FWYHTHJ.a8ZC", // 123
+    senha: "$2a$05$M1COhDus0bY6rqkD2REl8.ZccrFgzFXH0a7sOEYU/FWYHTHJ.a8ZC", // "123"
     nome: "Eduardo Arruda",
     perfis: "administrador",
     refPerfilAdministrador: null,
@@ -20,5 +20,5 @@ db.usuarios.save({
 
 db.clientes.save({
     nome: "adocoes.app",
-    secret: "$2a$05$M1COhDus0bY6rqkD2REl8.ZccrFgzFXH0a7sOEYU/FWYHTHJ.a8ZC" // 123
+    secret: "$2a$05$rK1K/Vy0z/Kj5INVusw4eeA3M0UJTHmEx.U5w1k2WTWT78NeeAnrK" // "407a4d80fce791751cd83ab1af3d9b26"
 });
