d8c1c45269a84607154277d72b353df26d01ec14
Eduardo Arruda <eduardo.arruda@doc.space>
2017-06-09 22:27:25 -0300

    Ajusta id da RFU09

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ server.get('/usuarios/:id_usuario/perfis', AuthManager.isAuthenticated, (request
     usuarioTranslator.getPerfilByUsuarioId(request, response)
 })
 
-// RFU07: PUT /usuarios/{id_usuario}/perfis
+// RFU09: PUT /usuarios/{id_usuario}/perfis
 server.put('/usuarios/:id_usuario/perfis', AuthManager.isAuthenticated, (request, response, next) => {
     const usuarioTranslator = new UsuarioTranslator()
     usuarioTranslator.updatePerfilUsuario(request, response)
